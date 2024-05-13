54b3c0e201a8983c8ef18b71d479934c6af79152
HopinNotDisaster <syq3315813594@gmail.com>
2024-05-13 19:10:57 +0800

    百度首页搜索三月七不会写！

---
+++ 1.百度首搜之三月七图片.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=&st=-1&fm=index&fr=&hs=0&xthttps=111110&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E4%B8%89%E6%9C%88%E4%B8%83'
+res = requests.get(url=url)
+res = res.content
+print(res)
